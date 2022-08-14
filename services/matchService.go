71b2078b1c70c03e2a5ab2d772fce42fe9973083
Tommaso Lauria <tommasolauria98@gmail.com>
2022-08-14 20:16:52 +0200

    feat: initializa board on creation

--- services/matchService.go
+++ services/matchService.go
@@ -13,16 +13,15 @@ func GetMatch(request GetMatchRequest) (GetMatchResponse, error) {
 }
 
 func CreateMatch(request CreateMatchRequest) (CreateMatchResponse, error) {
-	boardSize := 8
 	gameId, err := primitive.ObjectIDFromHex(request.GameId)
 	if err != nil {
 		return CreateMatchResponse{}, err
 	}
-	match := QuoridorMatch{
-		Id:     primitive.NewObjectID(),
-		GameId: gameId,
-		Board:  make([][]int, boardSize*2-1),
+	game, err := FindGame(gameId)
+	if err != nil {
+		return CreateMatchResponse{}, nil
 	}
+	match := newMatch(game)
 	err = InsertMatch(match)
 	if err != nil {
 		return CreateMatchResponse{}, err
@@ -34,4 +33,17 @@ func CreateMatch(request CreateMatchRequest) (CreateMatchResponse, error) {
 
 func Move(request MoveRequest) MoveResponse {
 	return MoveResponse{}
+}
+
+func newMatch(game Game) IMatch {
+	boardSize := 8
+	match := QuoridorMatch{
+		Id:     primitive.NewObjectID(),
+		GameId: game.Id,
+		Board:  make([][]int, boardSize*2-1),
+	}
+	for i := range match.Board {
+		match.Board[i] = make([]int, boardSize*2-1)
+	}
+	return match
 }
